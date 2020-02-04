e09ff1cfb982d57197d81dd0ece37aa49b3d781f
Juan Albarracin <jalbarracinq@gmail.com>
2020-02-04 17:25:50 +0100

    init

---
+++ src/main/java/com/lavamancer/bjazz/BjazzApplication.java
@@ -0,0 +1,13 @@
+package com.lavamancer.bjazz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjazzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjazzApplication.class, args);
+	}
+
+}
